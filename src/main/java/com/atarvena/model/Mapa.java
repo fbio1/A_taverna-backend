7ce060f05946693066b7fa7f1d265e11370983e6
Fábio Henrique <fbio1@gmail.com>
2019-05-27 12:59:24 -0300

    Criação das classes modelos

---
+++ src/main/java/com/atarvena/model/Mapa.java
@@ -0,0 +1,25 @@
+package com.atarvena.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Mapa extends AbstractModel<Integer> {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Override
+	public Integer getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+}
